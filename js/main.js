3fff0b3ea09ccbaa6df9fc4d6e31fc29615c3f35
Alexander Bienz <alex@alebie.com>
2015-09-14 23:05:41 +0100

    begun adding filter functionality

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 var Main = {
 	item_list: '',
+	item_filter: '',
 	API_URL: 'http://jsonplaceholder.typicode.com/posts',
+	json_data: '',
 
 	init: function(){
 		this.item_list = $('.js-item-list');
+		this.item_filter = $('.js-item-filter');
 		this.populateList();
 		this.addEvents();
 	},
@@ -12,6 +15,7 @@ var Main = {
 		var self = this;
 
 		$.getJSON(self.API_URL, function(data){
+			self.json_data = data;
 			$.each(data, function(index, item){
 				self.item_list.append('<a href="#" class="list-group-item" data-item-id="' + item.id + '">' + item.title + '</a>');
 			});
@@ -27,12 +31,27 @@ var Main = {
 			var clicked_link,
 					item_id;
 
+			e.preventDefault();
 			clicked_link = $(e.target);
 			item_id = clicked_link.data('item-id');
 			
 			self.item_list.find('a').removeClass('active');
 			clicked_link.addClass('active');
 			Livestax.store.set('item_id', item_id);
+		});
+
+		self.item_filter.on('keypress', function(){
+			var new_data = self.json_data.filter(function(obj){
+				if (obj.title.indexOf(self.item_filter.val()) > -1) {
+					return true;
+				} else {
+					return false;
+				};
+			});
+			self.item_list.empty();
+			$.each(new_data, function(index, item){
+				self.item_list.append('<a href="#" class="list-group-item" data-item-id="' + item.id + '">' + item.title + '</a>');
+			});
 		});
 	}
 }
