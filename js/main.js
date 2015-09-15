7a4457022f732928cbaef0379aea1f301a8d1ca4
Alexander Bienz <alex@alebie.com>
2015-09-15 11:32:07 +0100

    finished autocomplete feature

--- js/main.js
+++ js/main.js
@@ -40,7 +40,7 @@ var Main = {
 			Livestax.store.set('item_id', item_id);
 		});
 
-		self.item_filter.on('keypress', function(){
+		self.item_filter.on('keyup', function(){
 			var new_data = self.json_data.filter(function(obj){
 				if (obj.title.indexOf(self.item_filter.val()) > -1) {
 					return true;
@@ -50,7 +50,8 @@ var Main = {
 			});
 			self.item_list.empty();
 			$.each(new_data, function(index, item){
-				self.item_list.append('<a href="#" class="list-group-item" data-item-id="' + item.id + '">' + item.title + '</a>');
+				var item_title = item.title.replace(self.item_filter.val(), '<span class="text-primary">' + self.item_filter.val() + '</span>');
+				self.item_list.append('<a href="#" class="list-group-item" data-item-id="' + item.id + '">' + item_title + '</a>');
 			});
 		});
 	}
